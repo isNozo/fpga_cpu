e1fff83a5bca022ed92af51a664793b8553cb746
nozo <nozomi.pine@gmail.com>
2020-09-18 21:32:57 +0900

    Split cpu to multi stage

---
+++ src/main/alu.sv
@@ -0,0 +1,31 @@
+`include "lib_cpu.svh"
+
+module alu import lib_cpu :: *; (
+    input  OPECODE     opecode,
+    input  logic [3:0] imm,
+    input  logic [3:0] switch,
+    input  REGS        current,
+    output REGS        next
+);
+    always_comb begin
+        next = current;
+        next.cf = 1'b0;
+        next.ip = current.ip + 1;
+        
+        unique case (opecode)
+            ADD_A_IMM: {next.cf, next.a} = current.a + imm;
+            ADD_B_IMM: {next.cf, next.b} = current.b + imm;
+            MOV_A_IMM: next.a   = imm;
+            MOV_B_IMM: next.b   = imm;
+            MOV_A_B  : next.a   = current.b;
+            MOV_B_A  : next.b   = current.a;
+            JMP_IMM  : next.ip  = imm;
+            JNC_IMM  : next.ip  = current.cf ? current.ip + 1 : imm;
+            IN_A     : next.a   = switch;
+            IN_B     : next.b   = switch;
+            OUT_B    : next.out = current.b;
+            OUT_IMM  : next.out = imm;
+            default  : ;
+        endcase
+    end
+endmodule
--- src/main/cpu.sv
+++ src/main/cpu.sv
@@ -5,38 +5,14 @@ module cpu import lib_cpu :: *; (
     io_bus_if.master   io_bus,
     mem_bus_if.master  mem_bus
 );
-    REGS current, next;
-    
-    always_ff @(posedge ctrl_bus.clk) begin
-        if (~ctrl_bus.n_reset) current <= '0;
-        else                   current <= next;
-    end
-    
     OPECODE opecode;
     logic [3:0] imm;
     decoder decoder(.data(mem_bus.data), .opecode, .imm);
+    
+    REGS current, next;
     assign mem_bus.addr = current.ip;
     assign io_bus.led   = current.out;
     
-    always_comb begin
-        next = current;
-        next.cf = 1'b0;
-        next.ip = current.ip + 1;
-        
-        unique case (opecode)
-            ADD_A_IMM: {next.cf, next.a} = current.a + imm;
-            ADD_B_IMM: {next.cf, next.b} = current.b + imm;
-            MOV_A_IMM: next.a   = imm;
-            MOV_B_IMM: next.b   = imm;
-            MOV_A_B  : next.a   = current.b;
-            MOV_B_A  : next.b   = current.a;
-            JMP_IMM  : next.ip  = imm;
-            JNC_IMM  : next.ip  = current.cf ? current.ip + 1 : imm;
-            IN_A     : next.a   = io_bus.switch;
-            IN_B     : next.b   = io_bus.switch;
-            OUT_B    : next.out = current.b;
-            OUT_IMM  : next.out = imm;
-            default  : ;
-        endcase
-    end
+    alu alu(.opecode, .imm, .switch(io_bus.switch), .current, .next);
+    write_reg write_reg(.ctrl_bus, .next, .current);
 endmodule
