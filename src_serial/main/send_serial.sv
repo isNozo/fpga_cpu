ffed1b2f1a1093acc1c94d57c6267c5dc074f751
nozo <nozomi.pine@gmail.com>
2020-09-22 22:44:29 +0900

    Modify interface of send_seriral module.

--- src_serial/main/send_serial.sv
+++ src_serial/main/send_serial.sv
@@ -1,5 +1,8 @@
 module send_serial(
-    input  logic clk, rst,
-    output data_out
+    input  logic       clk, rst,
+    input  logic [7:0] data_in,
+    output logic       data_out,
+    input  logic       we,
+    output logic       busy
 );
 endmodule
--- src_serial/main/top.sv
+++ src_serial/main/top.sv
@@ -3,5 +3,17 @@ module top(
     input  logic ck_rst,
     output logic uart_rxd_out
 );
-    send_serial send_serial(.clk(CLK100MHZ), .rst(ck_rst), .data_out(uart_rxd_out));
+    logic [7:0] data_in;
+    assign data_in = 8'h41;
+    
+    logic we, busy;
+    
+    send_serial send_serial(
+        .clk(CLK100MHZ),
+        .rst(ck_rst),
+        .data_in,
+        .data_out(uart_rxd_out),
+        .we,
+        .busy
+    );
 endmodule
