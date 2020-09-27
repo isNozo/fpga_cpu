878ba069c727a337ebd7a7cee105dd98e1fdeb88
nozo <nozomi.pine@gmail.com>
2020-09-27 16:06:36 +0900

    Make top module.

--- src_serial/main/top.sv
+++ src_serial/main/top.sv
@@ -3,8 +3,15 @@ module top(
     input  logic rst,
     output logic uart_rxd_out
 );
+    typedef enum {
+        SEND,
+        WAIT,
+        FIN
+    } state;
+    
+    state       st, n_st;
     logic [7:0] data_in;
-    logic we, busy;
+    logic       we, busy;
     
     send_serial send_serial(
         .clk,
@@ -14,4 +21,28 @@ module top(
         .we,
         .busy
     );
+    
+    assign data_in = 8'h41;
+    
+    always_comb begin
+        we = 1'b0;
+        n_st = st;
+        
+        case (st)
+            SEND: begin
+                n_st = WAIT;
+                we = 1'b1;
+            end
+            WAIT: begin
+                if (~busy) n_st = FIN; 
+            end
+            FIN:;
+            default:;
+        endcase
+    end
+    
+    always_ff @(posedge clk) begin
+        if (~rst) st <= SEND;
+        else      st <= n_st;
+    end
 endmodule
--- src_serial/test/test_top.sv
+++ src_serial/test/test_top.sv
@@ -5,27 +5,19 @@ module test_top();
     
     top top(.*);
     
+    defparam top.send_serial.WAIT_DIV = 5;
+    
     always #5 clk = ~clk;
     initial   clk = 1'b0;
-    
+
     initial begin
         rst = 1'b0;
         #10;
         rst = 1'b1;
-    end
-    
-    defparam top.send_serial.WAIT_DIV = 5;
-    initial top.data_in = 8'b0101_0011;
-    
-    initial begin
-        top.we = 1'b0;
-        #50
-        top.we = 1'b1;
-        #20;
-        top.we = 1'b0;
         
+        #100;
         wait (top.busy == 1'b0);
-        #20;
+        #100;
         $finish;
     end
 endmodule
